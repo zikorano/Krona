3251b6ea0032e14fe50b8060bdc9164cbd3919ff
Zikora Ogbuagu <robbie89339@gmail.com>
2022-10-14 18:07:02 +0100

    Refactor: Modified variable names in `Vector2f.Rotatated()` Improvement: Added more math functions

--- src/KMath.cs
+++ src/KMath.cs
@@ -30,5 +30,11 @@ namespace Krona
         {
             return a + ((b - a) * weight);
         }
+
+        public static float DegToRad(float a) => a * (MathF.PI / 180.0f);
+        public static double DegToRad(double a) => a * (Math.PI / 180.0);
+        
+        public static float RadToDeg(float a) { return a * (180.0f / MathF.PI); }
+        public static double RadToDeg(double a) { return a * (180.0 / Math.PI); }
     }
 }
--- tests/Program.cs
+++ tests/Program.cs
@@ -5,5 +5,8 @@ class Program
 {
     public static void Main()
     {
+        var v = Vector2f.UP;
+        Console.WriteLine(v);
+        Console.WriteLine(v.Rotated(KMath.DegToRad(-90f)));
     }
 }
